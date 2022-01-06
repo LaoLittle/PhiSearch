be6e6a99985fad3522287306dd6356b5b9fce8b6
LaoLittle <y1941803875@outlook.com>
2022-01-06 17:15:25 +0800

    update

--- src/main/kotlin/PhiSearch.kt
+++ src/main/kotlin/PhiSearch.kt
@@ -1,13 +1,24 @@
 package org.laolittle.plugin
 
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.serialization.ExperimentalSerializationApi
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.contact.Contact.Companion.uploadImage
+import net.mamoe.mirai.event.globalEventChannel
+import net.mamoe.mirai.event.subscribeMessages
+import net.mamoe.mirai.message.data.PlainText
+import net.mamoe.mirai.message.data.buildMessageChain
+import net.mamoe.mirai.message.data.content
+import net.mamoe.mirai.message.nextMessage
 import net.mamoe.mirai.utils.info
 import org.laolittle.plugin.command.ConfigReload
-import org.laolittle.plugin.model.Searcher
+import org.laolittle.plugin.model.Song
 import org.laolittle.plugin.utils.init
+import org.laolittle.plugin.utils.search
+import org.laolittle.plugin.utils.songInfo
+import java.io.File
 
 @ExperimentalSerializationApi
 object PhiSearch : KotlinPlugin(
@@ -21,38 +32,62 @@ object PhiSearch : KotlinPlugin(
         logger.info { "加载完成" }
         init()
         ConfigReload.register()
-        Searcher.start()
-        /*GlobalEventChannel.subscribeMessages {
-            startsWith("测试"){
-                val date = LocalDate.now()
-                val random = Random(sender.id + date.year + date.monthValue + date.dayOfMonth).nextInt(1, 100)
-                subject.sendMessage("""
-                    ${songInfo[5]}
-                    测，都可以测
-                    $random
-                """.trimIndent())
-            }
-            /*startsWith("复读") {
-                if(isActivate(sender.id)) return@startsWith
-                subject.sendMessage("好")
-                whileSelectMessages {
-                    startsWith("停止") {
-                        isActivate = isActivate.minus(sender.id)
-                        false
+        globalEventChannel().subscribeMessages {
+            startsWith("定数查询") {
+                subject.sendMessage("请输入歌曲名称")
+                val name = runCatching { nextMessage(30_000) }.onFailure { subject.sendMessage("超时未输入") }
+                    .getOrElse { return@startsWith }
+                val info: List<Pair<Song, Int>> = songInfo.search(name.content)
+                when (info.size) {
+                    0 -> subject.sendMessage("未找到结果! ")
+                    1 -> {
+                        val result = buildMessageChain {
+                            add(PlainText("${info[0].first.name}\n"))
+                            add(PlainText("${info[0].first.description}\n"))
+                            val imageFile = img(info[0].second)
+                            if (imageFile.exists()) add(subject.uploadImage(imageFile))
+                        }
+                        subject.sendMessage(result)
                     }
-                    default {
-                        if (stop) return@default false
-                        val sourcemessage = this
-                        subject.sendMessage(sourcemessage.message)
-                        true
+                    else -> {
+                        subject.sendMessage(buildMessageChain {
+                            var order = 0
+                            info.forEach {
+                                order++
+                                add("$order${it.first.name}\n")
+                            }
+                            add("找到${order}个结果，请输入序号查询")
+                        })
+                        val number = try {
+                            nextMessage(12_000).content.toInt() - 1
+                        } catch (_: NumberFormatException) {
+                            subject.sendMessage("请输入数字 !")
+                            return@startsWith
+                        } catch (_: TimeoutCancellationException) {
+                            subject.sendMessage("超时未输入")
+                            return@startsWith
+                        }
+                        if (number > info.size - 1 || number < 0) subject.sendMessage("请输入正确的数字 !")
+                        else {
+                            val result = buildMessageChain {
+                                add(PlainText("${info[number].first.name}\n"))
+                                add(PlainText("${info[number].first.description}\n"))
+                                val imageFile = img(info[number].second)
+                                if (imageFile.exists()) add(subject.uploadImage(imageFile))
+                            }
+                            subject.sendMessage(result)
+                        }
                     }
                 }
-                subject.sendMessage("不复读了")
-            }*/
-        }*/
+            }
+        }
     }
 
     override fun onDisable() {
         logger.info { "卸载完成" }
+    }
+
+    private fun img(i: Int): File {
+        return File("${dataFolder}/SongImg").resolve("${i}.png")
     }
 }
